c76326fbbadbe83b6d76ad608c5d485526eba8f0
mininet <iwalsh@stanford.edu>
2015-05-16 12:37:06 -0700

    FatTree is working as its own module

--- fattree.py
+++ fattree.py
@@ -8,10 +8,13 @@ class FatTree(Topo):
 
     # Example names for k=4
     def __init__(self, k, link_bw, delay):
+        Topo.__init__(self)
         self.core_switches = {}
-        self.agg_switch = {}
+        self.agg_switches = {}
         self.edge_switches = {}
+#        self.build(k, link_bw, delay)
 
+#    def build(self, k, link_bw, delay):
         # Set up the (k/2)^2 core switches, c0 : c3
         core_switches = {}
         for i in xrange((k / 2) ** 2):
@@ -29,7 +32,7 @@ class FatTree(Topo):
 
                 # Each aggregation switch connects to k/2 core switches
                 for c in xrange(k / 2):
-                    core_switch = core_switches['c' + str(agg * (k / 2) + c)]
+                    core_switch = self.core_switches['c' + str(agg * (k / 2) + c)]
                     self.addLink(agg_switch, core_switch, bw=link_bw, delay=delay)
 
             # Second k/2 are edge switches, e0 : e1
@@ -40,7 +43,7 @@ class FatTree(Topo):
 
                 # Each edge switch connects to k/2 agg switches (all in pod)
                 for agg in xrange(k / 2):
-                    agg_switch = agg_switches['a' + str(pod * (k / 2) + agg)]
+                    agg_switch = self.agg_switches['a' + str(pod * (k / 2) + agg)]
                     self.addLink(edge_switch, agg_switch, bw=link_bw, delay=delay)
 
                 # Each edge switch connects to k/2 end hosts
